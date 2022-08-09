1e6d84d4eb77249b4fcc98eaaa112f7c76ef86c5
bitWolfy <anon.wolfy@pm.me>
2022-08-09 09:42:26 -0500

    [TagImplications] Add unit tests

--- test/TagAliases.test.js
+++ test/TagAliases.test.js
@@ -44,3 +44,48 @@ describe("TagAliases", () => {
         expect(result.data[0].status).toBe(esix.TagAliases.AliasStatus.Deleted);
     });
 });
+
+describe("TagImplications", () => {
+    // find()
+    test("Fetch implications (by name)", async () => {
+        const result = await esix.TagImplications.find({ name_matches: "mammal" });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBeGreaterThan(1);
+        expect(result.data[0].consequent_name).toBe("mammal");
+    });
+    test("Fetch implications (by antecedent)", async () => {
+        const result = await esix.TagImplications.find({ antecedent_name: "aardvark" });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBe(1);
+        expect(result.data[0].antecedent_name).toBe("aardvark");
+    });
+    test("Fetch implications (by consequent)", async () => {
+        const result = await esix.TagImplications.find({ consequent_name: "mammal" });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBeGreaterThan(1);
+        expect(result.data[0].consequent_name).toBe("mammal");
+    });
+    test("Fetch implications (by creator name)", async () => {
+        const result = await esix.TagImplications.find({ creator_name: "bitWolfy" });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBeGreaterThan(1);
+        expect(result.data[0].creator_id).toBe(211960);
+    });
+    test("Fetch implications (by approver name)", async () => {
+        const result = await esix.TagImplications.find({ approver_name: "bitWolfy" });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBeGreaterThan(1);
+        expect(result.data[0].approver_id).toBe(211960);
+    });
+    test("Fetch implications (by category)", async () => {
+        const result = await esix.TagImplications.find({ antecedent_tag_category: 4, consequent_tag_category: 3 });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBeGreaterThan(1);
+    });
+    test("Fetch implications (by status)", async () => {
+        const result = await esix.TagImplications.find({ status: esix.TagAliases.AliasStatus.Deleted });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBe(75);
+        expect(result.data[0].status).toBe(esix.TagAliases.AliasStatus.Deleted);
+    });
+});
