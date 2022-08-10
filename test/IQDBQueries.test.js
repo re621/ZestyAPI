f7484f7e770a13f1eae32067edb647aa6d9f4dc2
bitWolfy <anon.wolfy@pm.me>
2022-08-10 13:59:29 -0500

    [IQDB] Don't run tests when building

--- test/IQDBQueries.test.js
+++ test/IQDBQueries.test.js
@@ -1,18 +1,21 @@
 const E621 = require("./_tests");
+require('dotenv').config()
+
+const testIf = (condition, ...args) => condition ? test(...args) : test.skip(...args);
 
 describe("IQDBQueries", () => {
     // find()
-    test("Check for duplicates (by URL)", async () => {
+    testIf(process.env.TEST_IQDB == "true", "Check for duplicates (by URL)", async () => {
         const result = await E621.IQDBQueries.find({ url: "https://pbs.twimg.com/media/EcWMQ9IXsAAKoPE?format=jpg&name=orig" });
         expect(result.status.code).toBe(200);
         expect(result.data.length).toBe(1);
     });
-    test("Check for duplicates (by ID)", async () => {
+    testIf(process.env.TEST_IQDB == "true", "Check for duplicates (by ID)", async () => {
         const result = await E621.IQDBQueries.find({ post_id: 2626544 });
         expect(result.status.code).toBe(200);
         expect(result.data.length).toBe(2);
     });
-    test("Check for duplicates (invalid)", async () => {
+    testIf(process.env.TEST_IQDB == "true", "Check for duplicates (invalid)", async () => {
         const result = await E621.IQDBQueries.find();
         expect(result.status.code).toBe(490);
         expect(result.data).toStrictEqual([]);
