e2f35915e0d49b48cfcd710f160da086ad606b54
bitWolfy <anon.wolfy@pm.me>
2022-08-09 20:33:05 -0500

    [Blips] Test the derived search params

--- test/Blips.test.js
+++ test/Blips.test.js
@@ -7,7 +7,7 @@ describe("Blips", () => {
         expect(result.status.code).toBe(200);
         expect(result.data.length).toBe(75);
     });
-    test("Fetch blips (by name)", async () => {
+    test("Fetch blips (by creator name)", async () => {
         const result = await esix.Blips.find({ creator_name: "bitWolfy" });
         expect(result.status.code).toBe(200);
         expect(result.data.length).toBeGreaterThan(1);
@@ -23,10 +23,22 @@ describe("Blips", () => {
         expect(result.status.code).toBe(200);
         expect(result.data.length).toBeGreaterThanOrEqual(1);
     });
-    test("Fetch blips (reordered)", async () => {
-        const result = await esix.Blips.find({ order: "id_desc" });
+    test("Fetch blips (by ID)", async () => {
+        const result = await esix.Blips.find({ id: 12345 });
         expect(result.status.code).toBe(200);
-        expect(result.data.length).toBe(75);
+        expect(result.data.length).toBe(1);
+        expect(result.data[0].id).toBe(12345);
+    });
+    test("Fetch blips (by creator ID)", async () => {
+        const result = await esix.Blips.find({ creator_id: 211960 });
+        expect(result.status.code).toBe(200);
+        expect(result.data.length).toBeGreaterThan(1);
+        expect(result.data[0].creator_id).toBe(211960);
+    });
+    test("Fetch blips (404)", async () => {
+        const result = await esix.Blips.find({ creator_name: "abcdefg" });
+        expect(result.status.code).toBe(404);
+        expect(result.data.length).toBe(0);
     });
 
 });
